46ccdd61c8a8475867f417de9f6f528410cc1ba3
Nikita Krenov <nikitabaram@gmail.com>
2024-04-25 20:22:04 +0200

    transportyvanna

--- main.cpp
+++ main.cpp
@@ -3,7 +3,15 @@
 using namespace std;
 
 int main() {
-    
+    double amout;
+
+    cout << "Enter a fractional number: ";
+    cin >> amout;
+    cout << "" << endl;
+
+    int hryvnia = static_cast<int>(amout);
+    int kopecks = static_cast<int>(round((amout - hryvnia) * 100));
+    cout << hryvnia << " grn. " << kopecks << " kop." << endl;
 
     system("pause");
     return 0;
