8818705072757e34f15005abe6b1f5fd268e2fdf
Nikita Krenov <nikitabaram@gmail.com>
2024-04-25 19:41:36 +0200

    complete ex_4 exam for city

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int main() {
-    
+    int scale, distance;
+
+	cout << "Map scale (amount of km in 1 cm): ";   
+	cin >> scale;
+
+	cout << "Distance between dots that represent settlements on map (cm):";  
+	cin >> distance;
+
+	cout << scale * distance;
+	cout << " km" << endl;
 
     system("pause");
     return 0;
