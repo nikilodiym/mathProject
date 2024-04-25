bcea7f28067bced4d030757f91b7ac44d11a32a1
Nikita Krenov <nikitabaram@gmail.com>
2024-04-25 19:22:12 +0200

    for amout sum and grn and kop...

--- main.cpp
+++ main.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int main() {
-    
+    int grn, kop;
+    cout << "Enter the amount in hryvnias: ";
+    cin >> grn;
+    cout << "" << endl;
+    cout << "Enter the amount in kopiyka: ";
+    cin >> kop;
+    cout << "" << endl;
+
+    int breeding = kop / 100;
+    grn += breeding;
+    kop %= 100;
+
+    cout << "Finall amout in grn is: " << grn << " grn. And in the kopiyka is: " << kop << " kop" << endl;
+    cout << "Sum: " << grn << "." << kop << " grn" << endl;
+
 
     system("pause");
     return 0;
