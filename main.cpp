09f876cd1df030aa2d9533319ab7e4a4cfab2ce9
Nikita Krenov <nikitabaram@gmail.com>
2024-04-25 20:41:55 +0200

    lats answer so.. in this file we can see days which

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int main() {
-    
+    int days;
+
+    cout << "Enter the number of days: ";
+    cin >> days;
+    cout << "" << endl;
+
+    int weeks = days / 7;
+    int remaining_days = days % 7;
+
+    cout << "Weeks: " << weeks << "       Remaining days: " << remaining_days << endl;
 
     system("pause");
     return 0;
