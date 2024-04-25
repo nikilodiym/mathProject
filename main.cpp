3a561f79481a0a606b64f58933b8bd4373279a92
Nikita Krenov <nikitabaram@gmail.com>
2024-04-25 19:31:09 +0200

    ex_3 cmplete in this file we can see volume figure

--- main.cpp
+++ main.cpp
@@ -3,7 +3,23 @@
 using namespace std;
 
 int main() {
-    
+    int length, width, height;
+
+    cout << "Enter lenth pralelepipeda: ";
+    cin >> length;
+    cout << "" << endl;
+
+    cout << "Enter width pralelepipeda: ";
+    cin >> width;
+    cout << "" << endl;
+
+    cout << "Enter height pralelepipeda: ";
+    cin >> height;
+    cout << "" << endl;
+
+    int volume = length * width * height;
+    cout << "Volume pralelepipeda is: " << volume << "kva/sm" << endl;
+
 
     system("pause");
     return 0;
