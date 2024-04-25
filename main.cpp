52f0a0501a234b43ee4602d5ba3e110640067245
Nikita Krenov <nikitabaram@gmail.com>
2024-04-25 19:49:41 +0200

    sphere volume ex_5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <Windows.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 using namespace std;
 
 int main() {
-    
+    int radius;
+
+    cout << "Radius of sphere: ";
+    cin >> radius;
+    cout << "" << endl;
+    cout << (4 * M_PI * (radius * radius * radius)) / 3;
+    cout << "cm" << endl;
 
     system("pause");
     return 0;
