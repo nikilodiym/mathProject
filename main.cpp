cd24a8698a7256b50c3e28f7f078e473e62e195d
Nikita Krenov <nikitabaram@gmail.com>
2024-04-25 20:34:49 +0200

    s and v and i runner speed found ex_3

--- main.cpp
+++ main.cpp
@@ -3,7 +3,17 @@
 using namespace std;
 
 int main() {
-    
+    int i, t, v;
+
+    cout << "Enter the path the runner ran: ";
+    cin >> i;
+    cout << "" << endl;
+    cout << "Enter the time for which the runner covered the distance: ";
+    cin >> t;
+    cout << "" << endl;
+
+    int resuM = v = i / t;
+    cout << "The runner ran at speed: " << resuM << "km/h" << endl;
 
     system("pause");
     return 0;
