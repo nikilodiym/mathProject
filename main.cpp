7e3a0c8f26a3b87f8a08afcddbdd42f7ee5f7af1
Nikita Krenov <nikitabaram@gmail.com>
2024-04-25 19:09:05 +0200

    firts work about days in year

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <Windows.h>
+using namespace std;
 
 int main() {
     
+    int year;
+    const char* days_str[] = { "365", "366" };
+
+    cout << "Enter the year: ";
+    cin >> year;
+    cout << "" << endl;
+
+    int part = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    int day = part;
+    cout << "In " << year << "p have are " << days_str[day] << "days." << endl;
 
     system("pause");
     return 0;
