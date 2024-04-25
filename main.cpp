fb303268ea689f2d64b047fa36616e975cc9d12a
Nikita Krenov <nikitabaram@gmail.com>
2024-04-25 20:06:07 +0200

    firts hm ex_1 in this file we see time

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <Windows.h>
+using namespace std;
 
 int main() {
-    
+    int sec;
+
+    cout << "Enter time in the second: ";
+    cin >> sec;
+    cout << "" << endl;
+
+    int h = sec / 3600;
+    int m = (sec % 3600) / 60;
+    int s = sec % 60;
+
+    cout << "Now we have " << h << " hour " << m << " minute " << s << " second" << endl;
+    cout << h << "h" << m << "m" << s << endl;
 
     system("pause");
     return 0;
